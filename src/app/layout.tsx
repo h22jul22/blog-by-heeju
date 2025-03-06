21c3a34d6c46a56f226e4b84c938829de59cd9bf
h22jul22 <164333745+h22jul22@users.noreply.github.com>
2025-03-06 13:24:42 +0900

    prettier, prettier-plugin-tailwindcss 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,28 +3,28 @@ import './globals.css';
 import localFont from 'next/font/local';
 
 const moveSans = localFont({
-    src: '../../public/fonts/MoveSans-Medium.ttf',
-    variable: '--font-moveSans',
+  src: '../../public/fonts/MoveSans-Medium.ttf',
+  variable: '--font-moveSans',
 });
 
 const moveSansBold = localFont({
-    src: '../../public/fonts/MoveSans-Bold.ttf',
-    variable: '--font-moveSans-bold',
+  src: '../../public/fonts/MoveSans-Bold.ttf',
+  variable: '--font-moveSans-bold',
 });
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang='ko'>
-            <body className={`${moveSans.variable} ${moveSansBold.variable}`}>
-                <header>
-                    <NavBar />
-                </header>
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang='ko'>
+      <body className={`${moveSans.variable} ${moveSansBold.variable}`}>
+        <header>
+          <NavBar />
+        </header>
+        {children}
+      </body>
+    </html>
+  );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,19 +2,19 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default function NavBar() {
-    return (
-        <div className='flex items-center h-16 mx-4'>
-            <Link href='/' className='flex items-center'>
-                <span className='font-moveSans text-lx'>stocKING</span>
-                <Image
-                    src='/icons/king.svg'
-                    width={24}
-                    height={24}
-                    alt='왕관 아이콘'
-                    title='왕관 아이콘 (제작자: Rahul Kaklotar - Flaticon)'
-                />
-            </Link>
-            <div className='flex-grow'></div>
-        </div>
-    );
+  return (
+    <div className='mx-4 flex h-16 items-center'>
+      <Link href='/' className='flex items-center'>
+        <span className='font-moveSans text-lx'>stocKING</span>
+        <Image
+          src='/icons/king.svg'
+          width={24}
+          height={24}
+          alt='왕관 아이콘'
+          title='왕관 아이콘 (제작자: Rahul Kaklotar - Flaticon)'
+        />
+      </Link>
+      <div className='flex-grow'></div>
+    </div>
+  );
 }
