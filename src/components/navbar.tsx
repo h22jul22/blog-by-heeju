3302af7a7641230be20b1463411a779a3207d915
h22jul22 <164333745+h22jul22@users.noreply.github.com>
2025-03-09 23:12:23 +0900

    style: navbar 스타일 수정

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,15 +4,8 @@ import Link from 'next/link';
 export default function NavBar() {
   return (
     <div className='mx-4 flex h-16 items-center'>
-      <Link href='/' className='flex items-center'>
-        <span className='font-moveSans text-xl'>Hottest</span>
-        <Image
-          src='/icons/king.svg'
-          width={26}
-          height={26}
-          alt='왕관 아이콘'
-          title='왕관 아이콘 (제작자: Rahul Kaklotar - Flaticon)'
-        />
+      <Link href='/'>
+        <Image src='/icons/justory.jpeg' width={86} height={34} alt='justory 로고' />
       </Link>
       <div className='flex-grow'></div>
     </div>
