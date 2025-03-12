772d4ec25f20e6801c6da786aeb62f86e5f94752
h22jul22 <164333745+h22jul22@users.noreply.github.com>
2025-03-12 15:05:01 +0900

    zustand 설치 및 li태그 변경

--- src/components/sidebar/sidebar-index.tsx
+++ src/components/sidebar/sidebar-index.tsx
@@ -1,12 +1,12 @@
 export default function SidebarIndex() {
   const INDEX_UNDERLINE_STYLE = 'cursor-pointer border-b-2 border-white/30 hover:border-white';
   return (
-    <div className='flex flex-col items-center justify-center gap-4'>
-      <span className={INDEX_UNDERLINE_STYLE}>JavaScript</span>
-      <span className={INDEX_UNDERLINE_STYLE}>React.js</span>
-      <span className={INDEX_UNDERLINE_STYLE}>Next.js</span>
-      <span className={INDEX_UNDERLINE_STYLE}>Project</span>
-      <span className={INDEX_UNDERLINE_STYLE}>etc</span>
-    </div>
+    <ul className='flex flex-col items-center justify-center gap-4'>
+      <li className={INDEX_UNDERLINE_STYLE}>JavaScript</li>
+      <li className={INDEX_UNDERLINE_STYLE}>React.js</li>
+      <li className={INDEX_UNDERLINE_STYLE}>Next.js</li>
+      <li className={INDEX_UNDERLINE_STYLE}>Project</li>
+      <li className={INDEX_UNDERLINE_STYLE}>etc</li>
+    </ul>
   );
 }
