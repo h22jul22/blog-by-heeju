618ae0f8fb9a4b3ac496ec84842b7e21d8f2cc33
h22jul22 <164333745+h22jul22@users.noreply.github.com>
2025-03-14 22:56:52 +0900

    style: 버튼 hover 효과 적용

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,18 @@
 import { MdMenu, MdLightMode, MdOutlineSearch } from 'react-icons/md';
 
 export default function NavBar() {
+  const BUTTON_STYLE =
+    'h-full cursor-pointer p-3 border-r border-gray-200 hover:bg-linear-to-r from-transparent via-gray-100 to-[#ECEEF0]';
   return (
-    <div className='flex h-16 items-center justify-start border-b border-gray-100 shadow-sm'>
-      <button className='h-full cursor-pointer p-3'>
-        <MdMenu size={24} fill='gray' />
+    <div className='flex h-[75px] items-center justify-start border-b border-gray-100 shadow-sm'>
+      <button className={BUTTON_STYLE}>
+        <MdMenu size={24} fill='#a7a7a7' />
       </button>
-      <button className='h-full cursor-pointer p-3'>
-        <MdLightMode size={24} fill='gray' />
+      <button className={BUTTON_STYLE}>
+        <MdLightMode size={24} fill='#a7a7a7' />
       </button>
-      <button className='h-full cursor-pointer p-3'>
-        <MdOutlineSearch size={24} fill='gray' />
+      <button className={BUTTON_STYLE}>
+        <MdOutlineSearch size={24} fill='#a7a7a7' />
       </button>
     </div>
   );
